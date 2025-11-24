aa3cde6e3a51b3d315a91d43e25ae0ba15f006a1
AntagC01 <adilsonneto01@hotmail.com>
2025-11-24 14:15:17 -0300

    melhorias

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const GOIABAS = document.getElementById("Goiabas");
 const GOIABASDPS = document.getElementById("Goiabasdps");
 const CHICOS = document.getElementById("Chicos");
 const btnchicos = document.getElementById("Chico");
+const ZELELES = document.getElementById("Zeleles");
+const btnZELELES = document.getElementById("Zelele");
 const ROSINHAS = document.getElementById("Rosinhas");
 const btnrosinha = document.getElementById("Rosinha");
+
+const ROSAS = document.getElementById("Rosas");
+const ROSASDPS = document.getElementById("Rosasdps");
+const rosasdiv = document.getElementById("rosasdiv");
 
 const btnConfig = document.getElementById('btnConfig');
 const configWindow = document.getElementById('configWindow');
@@ -28,6 +34,8 @@ function setCookie(name, value) {
 // Inicializa GOB a partir do cookie
 let GOB = parseInt(getCookie("gobValue")) || 0;
 GOIABAS.textContent = "Goiabas: " + GOB;
+let ROSA = parseInt(getCookie("rosaValue")) || 0;
+GOIABAS.textContent = "Rosas: " + ROSA;
 
 // Inicializa CBT a partir do cookie
 let CBT = parseInt(getCookie("cbtValue")) || 0;
@@ -35,9 +43,23 @@ CHICOS.textContent = CBT + " Chicos";
 let custochico = parseInt(getCookie("custochicoValue")) || 10;
 btnchicos.textContent = "+1 Chico " + custochico + " Goiabas";
 
+// Inicializa ZLL a partir do cookie
+let ZLL = parseInt(getCookie("zllValue")) || 0;
+ZELELES.textContent = ZLL + " Ze leles";
+let custozelele = parseInt(getCookie("custozeleleValue")) || 100;
+btnZELELES.textContent = "+1 Ze lele " + custozelele + " Goiabas";
+let ZLL_BONUS = parseInt(getCookie("zllbonusValue")) || 7;
+
 // Inicializa RS a partir do cookie
 let RS = parseInt(getCookie("rsValue")) || 0;
 ROSINHAS.textContent = RS + " Rosinhas";
+let custorosinha = parseInt(getCookie("custorosinhaValue")) || 1000;
+btnrosinha.textContent = "+1 Rosinha " + custorosinha + " Goiabas";
+
+if (RS > 0){
+    rosasdiv.style.display = 'block';
+}
+
 
 
 // Lista de áudios com peso
@@ -101,23 +123,58 @@ function escolherAudioPonderado(audios) {
         }
     };
 
-    document.getElementById("Rosinha").onclick = function () {
-        RS += 1;
-        ROSINHAS.textContent = RS + " Rosinhas";
-        setCookie("rsValue", RS);
-        // nenhum som aqui
+       document.getElementById("Zelele").onclick = function () {
+        if (GOB >= custozelele){
+            GOB -= custozelele;
+            GOIABAS.textContent = "Goiabas: " + GOB;
+            setCookie("gobValue", GOB);
 
+            ZLL += 1;
+            custozelele = Math.trunc(custozelele * 1.25);
+            ZELELES.textContent = ZLL + " Ze leles";
+            btnZELELES.textContent = "+1 Ze lele " + custozelele + " Goiabas";
+
+            setCookie("zllValue", ZLL);
+            setCookie("custozeleleValue", custozelele);
+
+            audioPlayer.src = "";
+            audioPlayer.volume = volumeControl.value;
+            audioPlayer.play();
+        }
+    };
+
+    document.getElementById("Rosinha").onclick = function () {
+        if (GOB >= custorosinha){
+            GOB -= custorosinha;
+            GOIABAS.textContent = "Goiabas: " + GOB;
+            setCookie("gobValue", GOB);
+
+            RS += 1;
+            custorosinha = Math.trunc(custorosinha * 1.25);
+            ROSINHAS.textContent = RS + " Rosinhas";
+            btnrosinha.textContent = "+1 Rosinha " + custorosinha + " Goiabas";
+
+            setCookie("rsValue", RS);
+            setCookie("custorosinhaValue", custorosinha);
+            rosasdiv.style.display = 'block';
         audioPlayer.src = "mp3/rosinha-meu-amor-1763995355051.mp3";
         audioPlayer.volume = volumeControl.value;
         audioPlayer.play();
     };
+};
 
 setInterval(() => {
-    GOB += CBT;
-    GOBDPS = CBT;
+    GOB += CBT + (ZLL * ZLL_BONUS);
+    GOBDPS = CBT + (ZLL * ZLL_BONUS);
     GOIABAS.textContent = "Goiabas: " + GOB;
     setCookie("gobValue", GOB);
     GOIABASDPS.textContent = "Goiabas per second: " + GOBDPS;
+
+    ROSA += RS;
+    ROSADPS = RS;
+    ROSAS.textContent = "Rosas: " + ROSA;
+    setCookie("rosaValue", ROSA);
+    ROSASDPS.textContent = "Rosas per second: " + ROSADPS;
 }, 1000);
 
 
@@ -143,16 +200,26 @@ volumeControl.addEventListener('input', () => {
         CBT = 0;
         GOB = 0;
         custochico = 10;
+        ZLL = 0;
+        custozelele = 100;
+        RS = 0;
+        custorosinha = 1000;
         setCookie("gobValue", 0);
         setCookie("cbtValue", 0);
         setCookie("custochicoValue", 10); // valor inicial do Chico
+        setCookie("zllValue", 0);
+        setCookie("custozeleleValue", 100); // valor inicial do Chico
         setCookie("rsValue", 0);
+        setCookie("custorosinhaValue", 1000); // valor inicial do Chico
 
         // Atualiza os elementos da tela
         document.getElementById("Goiabas").textContent = "Goiabas: 0";
         document.getElementById("Chicos").textContent = "0 Chicos";
-        document.getElementById("Rosinhas").textContent = "0 Rosinhas";
         document.getElementById("Chico").textContent = "+1 Chico 10 Goiabas";
+        document.getElementById("Zeleles").textContent = "0 Ze leles";
+        document.getElementById("Zelele").textContent = "+1 Ze lele 100 Goiabas";
+        document.getElementById("Rosinhas").textContent = "0 Rosinhas";
+        document.getElementById("Rosinha").textContent = "+1 Rosinha 1000 Goiabas";
 
         // Se você quiser, também pode resetar contadores por segundo
         // e qualquer outro estado do jogo
