71d6183f27bdaff2765fee59a4da363a41cdbe44
AntagC01 <adilsonneto01@hotmail.com>
2025-12-03 09:49:37 -0300

    teste

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let CBTMA = parseInt(getCookie("cbtmaValue")) || 0;
     let custochicomalhado = parseInt(getCookie("custochicomalhadoValue")) || 10000000;
-    let CBTMA_BONUS = parseInt(getCookie("cbtmabonusValue")) || 343;
+    let CBTMA_BONUS = parseInt(getCookie("cbtmabonusValue")) || 100;
 
     let GLOBAL = parseFloat(getCookie("globalbonusValue")) || 1;
     let GLOBAL_ROSA = parseFloat(getCookie("globalbonusrosaValue")) || 1;
@@ -229,10 +229,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("Aperta").onclick = function () {
         GOB += CLICK;
         if (lingua == 'en') {
-        GOIABAS.textContent = "Guavas: " + GOB;
+        GOIABAS.textContent = "Guavas: " + GOB.toLocaleString('en');
         }
         if (lingua == 'pt') {
-        GOIABAS.textContent = "Goiabas: " + GOB;
+        GOIABAS.textContent = "Goiabas: " + GOB.toLocaleString('pt-BR');
         }
         setCookie("gobValue", GOB);
 
@@ -256,11 +256,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (lingua == 'en') {
         CHICOS.textContent = CBT + " Chicos";
-        btnchicos.textContent = "+1 Chico " + custochico + " Guavas";
+        btnchicos.textContent = "+1 Chico " + custochico.toLocaleString('en') + " Guavas";
         }
         if (lingua == 'pt') {
         CHICOS.textContent = CBT + " Chicos";
-        btnchicos.textContent = "+1 Chico " + custochico + " Goiabas";
+        btnchicos.textContent = "+1 Chico " + custochico.toLocaleString('pt-BR') + " Goiabas";
         }
 
         setCookie("gobValue", GOB);
@@ -287,11 +287,11 @@ document.addEventListener("DOMContentLoaded", () => {
         
         if (lingua == 'en') {
         ZELELES.textContent = ZLL + " Ze leles";
-        btnZELELES.textContent = "+1 Ze lele " + custozelele + " Guavas";
+        btnZELELES.textContent = "+1 Ze lele " + custozelele.toLocaleString('en') + " Guavas";
         }
         if (lingua == 'pt') {
         ZELELES.textContent = ZLL + " Ze leles";
-        btnZELELES.textContent = "+1 Ze lele " + custozelele + " Goiabas";
+        btnZELELES.textContent = "+1 Ze lele " + custozelele.toLocaleString('pt-BR') + " Goiabas";
         }
 
         setCookie("gobValue", GOB);
@@ -317,11 +317,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (lingua == 'en') {
         ROSINHAS.textContent = RS + " Rosinhas";
-        btnrosinha.textContent = "+1 Rosinha " + custorosinha + " Guavas";
+        btnrosinha.textContent = "+1 Rosinha " + custorosinha.toLocaleString('en') + " Guavas";
         }
         if (lingua == 'pt') {
         ROSINHAS.textContent = RS + " Rosinhas";
-        btnrosinha.textContent = "+1 Rosinha " + custorosinha + " Goiabas";
+        btnrosinha.textContent = "+1 Rosinha " + custorosinha.toLocaleString('pt-BR') + " Goiabas";
         }
 
         setCookie("gobValue", GOB);
@@ -349,11 +349,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (lingua == 'en') {
         CAVALOS.textContent = CVL + " Cavalos";
-        btncavalo.textContent = "+1 Cavalo " + custocavalo + " Roses";
+        btncavalo.textContent = "+1 Cavalo " + custocavalo.toLocaleString('en') + " Roses";
         }
         if (lingua == 'pt') {
         CAVALOS.textContent = CVL + " Cavalos";
-        btncavalo.textContent = "+1 Cavalo " + custocavalo + " Rosas";
+        btncavalo.textContent = "+1 Cavalo " + custocavalo.toLocaleString('pt-BR') + " Rosas";
         }
 
         setCookie("rosaValue", ROSA);
@@ -382,11 +382,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (lingua == 'en') {
         CHICOSMA.textContent = CBTMA + " Chicos Bulked";
-        btnchicosMA.textContent = "+1 Chico Bulked " + custochicomalhado + " Guavas";
+        btnchicosMA.textContent = "+1 Chico Bulked " + custochicomalhado.toLocaleString('en') + " Guavas";
         }
         if (lingua == 'pt') {
         CHICOSMA.textContent = CBTMA + " Chicos Malhados";
-        btnchicosMA.textContent = "+1 Chico malhado " + custochicomalhado + " Goiabas";
+        btnchicosMA.textContent = "+1 Chico malhado " + custochicomalhado.toLocaleString('pt-BR') + " Goiabas";
         }
 
         setCookie("gobValue", GOB);
@@ -408,17 +408,17 @@ document.addEventListener("DOMContentLoaded", () => {
             GOBDPS = Math.trunc(GOBDPS * (1 + (CBT * 0.02)));
         }
         if (up11 == 1){
-            GOBDPS = Math.trunc(GOBDPS * (1 + (ROSA * 0.0001)));
+            GOBDPS = Math.trunc(GOBDPS * (1 + (ROSA * 0.000001)));
         }
         GOB += GOBDPS;
 
         if (lingua == 'en') {
-        GOIABAS.textContent = "Guavas: " + GOB;
-        GOIABASDPS.textContent = "Guavas per second: " + GOBDPS;
+        GOIABAS.textContent = "Guavas: " + GOB.toLocaleString('en');
+        GOIABASDPS.textContent = "Guavas per second: " + GOBDPS.toLocaleString('en');
         }
         if (lingua == 'pt') {
-        GOIABAS.textContent = "Goiabas: " + GOB;
-        GOIABASDPS.textContent = "Goiabas por segundo: " + GOBDPS;
+        GOIABAS.textContent = "Goiabas: " + GOB.toLocaleString('pt-BR');
+        GOIABASDPS.textContent = "Goiabas por segundo: " + GOBDPS.toLocaleString('pt-BR');
         }
 
         setCookie("gobValue", GOB);
@@ -427,39 +427,39 @@ document.addEventListener("DOMContentLoaded", () => {
         ROSA += ROSADPS;
 
         if (lingua == 'en') {
-        ROSAS.textContent = "Roses: " + ROSA;
-        ROSASDPS.textContent = "Roses per second: " + ROSADPS;
+        ROSAS.textContent = "Roses: " + ROSA.toLocaleString('pt-BR');
+        ROSASDPS.textContent = "Roses per second: " + ROSADPS.toLocaleString('en');
         }
         if (lingua == 'pt') {
-        ROSAS.textContent = "Rosas: " + ROSA;
-        ROSASDPS.textContent = "Rosas por segundo: " + ROSADPS;
+        ROSAS.textContent = "Rosas: " + ROSA.toLocaleString('pt-BR');
+        ROSASDPS.textContent = "Rosas por segundo: " + ROSADPS.toLocaleString('pt-BR');
         }
 
         setCookie("rosaValue", ROSA);
 
         if (lingua == 'en') {
         CHICOS.textContent = CBT + " Chicos";
-        btnchicos.textContent = "+1 Chico " + custochico + " Guavas";
+        btnchicos.textContent = "+1 Chico " + custochico.toLocaleString('en') + " Guavas";
         ZELELES.textContent = ZLL + " Ze leles";
-        btnZELELES.textContent = "+1 Ze lele " + custozelele + " Guavas";
+        btnZELELES.textContent = "+1 Ze lele " + custozelele.toLocaleString('en') + " Guavas";
         ROSINHAS.textContent = RS + " Rosinhas";
-        btnrosinha.textContent = "+1 Rosinha " + custorosinha + " Guavas";
+        btnrosinha.textContent = "+1 Rosinha " + custorosinha.toLocaleString('en') + " Guavas";
         CAVALOS.textContent = CVL + " Cavalos";
-        btncavalo.textContent = "+1 Cavalo " + custocavalo + " Roses";
+        btncavalo.textContent = "+1 Cavalo " + custocavalo.toLocaleString('en') + " Roses";
         CHICOSMA.textContent = CBTMA + " Chico Bulked";
-        btnchicosMA.textContent = "+1 Chico bulked " + custochicomalhado + " Guavas";
+        btnchicosMA.textContent = "+1 Chico bulked " + custochicomalhado.toLocaleString('en') + " Guavas";
         }
         if (lingua == 'pt') {
         CHICOS.textContent = CBT + " Chicos";
-        btnchicos.textContent = "+1 Chico " + custochico + " Goiabas";
+        btnchicos.textContent = "+1 Chico " + custochico.toLocaleString('pt-BR') + " Goiabas";
         ZELELES.textContent = ZLL + " Ze leles";
-        btnZELELES.textContent = "+1 Ze lele " + custozelele + " Goiabas";
+        btnZELELES.textContent = "+1 Ze lele " + custozelele.toLocaleString('pt-BR') + " Goiabas";
         ROSINHAS.textContent = RS + " Rosinhas";
-        btnrosinha.textContent = "+1 Rosinha " + custorosinha + " Goiabas";
+        btnrosinha.textContent = "+1 Rosinha " + custorosinha.toLocaleString('pt-BR') + " Goiabas";
         CAVALOS.textContent = CVL + " Cavalos";
-        btncavalo.textContent = "+1 Cavalo " + custocavalo + " Rosas";
+        btncavalo.textContent = "+1 Cavalo " + custocavalo.toLocaleString('pt-BR') + " Rosas";
         CHICOSMA.textContent = CBTMA + " Chicos Malhados";
-        btnchicosMA.textContent = "+1 Chico malhado " + custochicomalhado + " Goiabas";
+        btnchicosMA.textContent = "+1 Chico malhado " + custochicomalhado.toLocaleString('pt-BR') + " Goiabas";
         }
     }, 1000);
 
@@ -763,7 +763,7 @@ upgrade7.textContent = "Punch the trees (10x guavas per click)";
 upgrade8.textContent = "Hunt the jaguar that ate Ze Lele (?)";
 upgrade9.textContent = "Feed the horses with guavas (10% more horse bonus)";
 upgrade10.textContent = 'Create a enterprise and manage the chicos (+2% GBPS per chico)'
-upgrade11.textContent = 'Invest the roses in the stockmarket (+1% GBPS per 1000 Roses)'
+upgrade11.textContent = 'Invest the roses in the stockmarket (+1% GBPS per 10000 Roses)'
 }
 if (lingua == 'pt') {
 FAZENDINHA.textContent = 'Fazendinha';
@@ -789,92 +789,132 @@ upgrade7.textContent = 'Soque as arvores (10x goiabas por clique)'
 upgrade8.textContent = 'Caçar a onça que comeu o Ze lele (?)'
 upgrade9.textContent = 'Alimentar os cavalos com goiabas (10% aumento no bonus dos cavalos)'
 upgrade10.textContent = 'Criar uma empresa e administrar os chicos (+2% GBPS per chico)'
-upgrade11.textContent = 'Inestir as rosas na bolsa (+1% GBPS por 1000 Rosas)'
+upgrade11.textContent = 'Inestir as rosas na bolsa (+1% GBPS por 10000 Rosas)'
 }
     }, 2500);
 
     document.getElementById("Upgrade1").onclick = function () {
-        if (GOB >= 80){
-            setCookie("up1Value", 1);
-            setCookie("cbtbonusValue", CBT_BONUS * 2);
-            CBT_BONUS = CBT_BONUS * 2;
-            up1 = 1;
-            upgrade1.style.display = 'none';
-    }
-};
+        upgrade1Func();
+    };
 
     document.getElementById("Upgrade2").onclick = function () {
-        if (ZLL >= 1){
-            setCookie("up2Value", 1);
-            setCookie("zllValue", ZLL - 1);
-            ZLL -= 1;
-            up2 = 1;
-            upgrade2.style.display = 'none';
-            ZELELES.textContent = ZLL + " Ze leles";
-            if (lingua == 'en') {
-            showCustomAlert("A jaguar came and ate Ze Lele :(")
-            }
-            if (lingua == 'pt') {
-            showCustomAlert("Uma onça veio e cumeu o Ze lele :(")
-            }
-    }
-};
+        upgrade2Func();
+    };
 
     document.getElementById("Upgrade3").onclick = function () {
-        if (GOB >= 500){
-            GOB -= 500;
-            setCookie("up3Value", 1);
-            setCookie("globalbonusValue", GLOBAL * 2);
-            GLOBAL = GLOBAL * 2;
-            up3 = 1;
-            upgrade3.style.display = 'none';
-    }
-};
+        upgrade3Func();
+    };
 
     document.getElementById("Upgrade4").onclick = function () {
-        if (ROSA >= 50){
-            ROSA -= 50;
-            setCookie("up4Value", 1);
-            setCookie("globalbonusValue", GLOBAL * 3);
-            GLOBAL = GLOBAL * 3;
-            up4 = 1;
-            upgrade4.style.display = 'none';
-    }
-};
+        upgrade4Func();
+    };
 
     document.getElementById("Upgrade5").onclick = function () {
-        if (RS >= 10){
-            setCookie("up5Value", 1);
-            setCookie("globalbonusValue", GLOBAL * 10);
-            GLOBAL = GLOBAL * 10;
-            up5 = 1;
-            upgrade5.style.display = 'none';
-    }
-};
+        upgrade5Func();
+    };
 
     document.getElementById("Upgrade6").onclick = function () {
-        if (GOB >= 30){
-            GOB -= 30;
-            setCookie("up6Value", 1);
-            setCookie("clickValue", CLICK * 2);
-            CLICK = CLICK * 2;
-            up6 = 1;
-            upgrade6.style.display = 'none';
-    }
-};
+        upgrade6Func();
+    };
 
     document.getElementById("Upgrade7").onclick = function () {
-        if (GOB >= 750){
-            GOB -= 750;
-            setCookie("up7Value", 1);
-            setCookie("clickValue", CLICK * 10);
-            CLICK = CLICK * 10;
-            up7 = 1;
-            upgrade7.style.display = 'none';
-    }
-};
+        upgrade7Func();
+    };
 
-document.getElementById("Upgrade8").onclick = function () {
+    document.getElementById("Upgrade8").onclick = function () {
+        upgrade8Func();
+    };
+
+    document.getElementById("Upgrade9").onclick = function () {
+        upgrade9Func();
+    };
+
+    document.getElementById("Upgrade10").onclick = function () {
+        upgrade10Func();
+    };
+
+    document.getElementById("Upgrade11").onclick = function () {
+        upgrade11Func();
+    };
+
+function upgrade1Func() {
+    if (GOB >= 80){
+        setCookie("up1Value", 1);
+        setCookie("cbtbonusValue", CBT_BONUS * 2);
+        CBT_BONUS *= 2;
+        up1 = 1;
+        upgrade1.style.display = 'none';
+    }
+}
+
+function upgrade2Func() {
+    if (ZLL >= 1){
+        setCookie("up2Value", 1);
+        setCookie("zllValue", ZLL - 1);
+        ZLL -= 1;
+        up2 = 1;
+        upgrade2.style.display = 'none';
+        ZELELES.textContent = ZLL + " Ze leles";
+        if (lingua == 'en') showCustomAlert("A jaguar came and ate Ze Lele :(");
+        if (lingua == 'pt') showCustomAlert("Uma onça veio e cumeu o Ze lele :(");
+    }
+}
+
+function upgrade3Func() {
+    if (GOB >= 500){
+        GOB -= 500;
+        setCookie("up3Value", 1);
+        setCookie("globalbonusValue", GLOBAL * 2);
+        GLOBAL *= 2;
+        up3 = 1;
+        upgrade3.style.display = 'none';
+    }
+}
+
+function upgrade4Func() {
+    if (ROSA >= 50){
+        ROSA -= 50;
+        setCookie("up4Value", 1);
+        setCookie("globalbonusValue", GLOBAL * 3);
+        GLOBAL *= 3;
+        up4 = 1;
+        upgrade4.style.display = 'none';
+    }
+}
+
+function upgrade5Func() {
+    if (RS >= 10){
+        setCookie("up5Value", 1);
+        setCookie("globalbonusValue", GLOBAL * 10);
+        GLOBAL *= 10;
+        up5 = 1;
+        upgrade5.style.display = 'none';
+    }
+}
+
+function upgrade6Func() {
+    if (GOB >= 30){
+        GOB -= 30;
+        setCookie("up6Value", 1);
+        setCookie("clickValue", CLICK * 2);
+        CLICK *= 2;
+        up6 = 1;
+        upgrade6.style.display = 'none';
+    }
+}
+
+function upgrade7Func() {
+    if (GOB >= 750){
+        GOB -= 750;
+        setCookie("up7Value", 1);
+        setCookie("clickValue", CLICK * 10);
+        CLICK *= 10;
+        up7 = 1;
+        upgrade7.style.display = 'none';
+    }
+}
+
+function upgrade8Func() {
     if (ZLL >= 15){
         setCookie("zllValue", ZLL + 1);
         ZLL += 1;
@@ -883,16 +923,12 @@ document.getElementById("Upgrade8").onclick = function () {
         setCookie("zllbonusValue", ZLL_BONUS);
         up8 = 1;
         upgrade8.style.display = 'none';
-        if (lingua == 'en') {
-        showCustomAlert("You beat the jaguar until it spit Ze Lele back :) (2x Ze Lele production)")
-        }
-        if (lingua == 'pt') {
-        showCustomAlert("Você bate na onça até ela cuspir o Ze lele de volta :) (2x Ze Lele produção)")
-        }
+        if (lingua == 'en') showCustomAlert("You beat the jaguar until it spit Ze Lele back :) (2x Ze Lele production)");
+        if (lingua == 'pt') showCustomAlert("Você bate na onça até ela cuspir o Ze lele de volta :) (2x Ze Lele produção)");
     }
-};
+}
 
-document.getElementById("Upgrade9").onclick = function () {
+function upgrade9Func() {
     if (GOB >= 100000){
         CVL_BONUS += 0.1;
         setCookie("cvlbonusValue", CVL_BONUS);
@@ -900,24 +936,42 @@ document.getElementById("Upgrade9").onclick = function () {
         up9 = 1;
         upgrade9.style.display = 'none';
     }
-};
+}
 
-document.getElementById("Upgrade10").onclick = function () {
+function upgrade10Func() {
     if (CBT >= 60){
         setCookie("up10Value", 1);
         up10 = 1;
         upgrade10.style.display = 'none';
     }
-};
+}
 
-document.getElementById("Upgrade11").onclick = function () {
+function upgrade11Func() {
     if (ROSA >= 100000){
         setCookie("up11Value", 1);
         up11 = 1;
         upgrade11.style.display = 'none';
     }
-};
+}
 
+document.getElementById("ApplyAllButton").onclick = function () {
+upgreideiatudo();
+}
+
+// Função para chamar todos os upgrades
+function upgreideiatudo() {
+    upgrade1Func();
+    upgrade2Func();
+    upgrade3Func();
+    upgrade4Func();
+    upgrade5Func();
+    upgrade6Func();
+    upgrade7Func();
+    upgrade8Func();
+    upgrade9Func();
+    upgrade10Func();
+    upgrade11Func();
+}
     function mostrarConquista(texto) {
         const popup = document.getElementById("conquistaPopup");
         const textoElemento = document.getElementById("conquistaTexto");
@@ -976,7 +1030,7 @@ document.getElementById("Upgrade11").onclick = function () {
         ROSADPS = 0;
         custorosinha = 1000;
         CBTMA = 0;
-        CBTMA_BONUS = 343;
+        CBTMA_BONUS = 100;
         custochicomalhado = 10000000;
         GLOBAL = 1;
         CLICK = 1;
@@ -1007,7 +1061,7 @@ document.getElementById("Upgrade11").onclick = function () {
         setCookie("rosaValue", 0);
         setCookie("cbtmaValue", 0);
         setCookie("custochicomalhadoValue", 10000000);
-        setCookie("cbtmabonusValue", 343);
+        setCookie("cbtmabonusValue", 100);
         setCookie("custorosinhaValue", 1000); // valor inicial do Chico
 
 
@@ -1070,7 +1124,7 @@ document.getElementById("Upgrade11").onclick = function () {
         ROSADPS = 0;
         custorosinha = 1000;
         CBTMA = 0;
-        CBTMA_BONUS = 343;
+        CBTMA_BONUS = 100;
         custochicomalhado = 10000000;
         GLOBAL = 1 + (ascendvar * 0.5); //bonus
         CLICK = 1 + (ascendvar); //bonus
@@ -1106,7 +1160,7 @@ document.getElementById("Upgrade11").onclick = function () {
         setCookie("custorosinhaValue", 1000); // valor inicial do Chico
         setCookie("cbtmaValue", 0);
         setCookie("custochicomalhadoValue", 10000000);
-        setCookie("cbtmabonusValue", 343);
+        setCookie("cbtmabonusValue", 100);
         setCookie("objetivoascendValue",objetivoascend)
 
 
